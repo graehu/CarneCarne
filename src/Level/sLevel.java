499ebf249777efbef45d444fce3efb54f77140bf
Graham <graehu@gmail.com>
2011-07-13 20:06:27 +0100

    level
    Just commiting and pushing so as I can work on the level at home... because I'm lonly sitting at uni.

--- src/Level/sLevel.java
+++ src/Level/sLevel.java
@@ -73,9 +73,9 @@ public class sLevel {
     public static void init() throws SlickException
     {
         //mTiledMap = new AnimatedTiledMap("assets/DeekTestMap.tmx"); /// Fuck off ass jerky
-        mTiledMap = new AnimatedTiledMap("assets/AaronTestMap.tmx");
+        //mTiledMap = new AnimatedTiledMap("assets/AaronTestMap.tmx");
         //mTiledMap = new AnimatedTiledMap("assets/Graham_Tutorial.tmx");
-        //mTiledMap = new AnimatedTiledMap("assets/platforms2.tmx");
+        mTiledMap = new AnimatedTiledMap("assets/platforms2.tmx");
         mTiledMap.initAnimationlayer("assets/TileAnimation.def");
         mLevelEditor = new LevelEditor(mTiledMap);
         midLayer = mTiledMap.getLayerIndex("Level");
