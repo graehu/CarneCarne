ea0aaa833c7466f34a4c66c37273cf277e7a9ef1
Graham <graehu@gmail.com>
2011-07-14 18:38:45 +0100

    Simple Race
    A race to be tested for the kids annnd some new hard tiles courtesy of gregums.

--- src/Level/sLevel.java
+++ src/Level/sLevel.java
@@ -74,9 +74,10 @@ public class sLevel {
     {
         //mTiledMap = new AnimatedTiledMap("assets/DeekTestMap.tmx"); /// Fuck off ass jerky
         //mTiledMap = new AnimatedTiledMap("assets/AaronTestMap.tmx");
-        mTiledMap = new AnimatedTiledMap("assets/DeekTestMap.tmx"); /// Nomnom tasty poocake
+        //mTiledMap = new AnimatedTiledMap("assets/DeekTestMap.tmx"); /// Nomnom tasty poocake
         //mTiledMap = new AnimatedTiledMap("assets/Graham_Tutorial.tmx");
         //mTiledMap = new AnimatedTiledMap("assets/platforms2.tmx");
+        mTiledMap = new AnimatedTiledMap("assets/simple_race.tmx");
         mTiledMap.initAnimationlayer("assets/TileAnimation.def");
         mLevelEditor = new LevelEditor(mTiledMap);
         midLayer = mTiledMap.getLayerIndex("Level");
