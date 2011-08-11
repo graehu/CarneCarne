3bb4e5ea73964c1cb6e327c7ff23b20a38ca0ad9
Graham <graehu@gmail.com>
2011-08-11 05:26:45 +0100

    For Deek
    silly deek.

---
+++ data/assets/Maps/Tilesets/Tilesets/TilePlaceAnimation.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TilePlaceAnimation" tilewidth="64" tileheight="64">
+ <image source="Images/Special/TilePlaceAnimation.png" width="256" height="64"/>
+ <tile id="2">
+  <properties>
+   <property name="animRef" value="acidbubble"/>
+   <property name="duriation" value="100"/>
+   <property name="maxTimeOffset" value="0"/>
+   <property name="randomiseSpeed" value="false"/>
+  </properties>
+ </tile>
+</tileset>
